b7c6dc12af8559a1b36452cb644d01b9cdec82c1
Ung Nguyen Song Phuc <songphucungnguyen@gmail.com>
2024-08-27 14:52:51 +0700

    v1.0.1b

--- FlashCardLearn/Commands/ChangeFinishingSoundCommand.cs
+++ FlashCardLearn/Commands/ChangeFinishingSoundCommand.cs
@@ -4,6 +4,7 @@ using Repositories.Models;
 using Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,11 +30,47 @@ namespace FlashCardLearn.Commands
             if(openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string selectedFilePath = openFileDialog.FileName;
-                Settings settings = new Settings()
+                if(IsValidWavFile(selectedFilePath))
                 {
-                    FinishingSoundFilePath = selectedFilePath
-                };
-                _settingViewModel.FinishingSoundFilePath = selectedFilePath;
+                    Settings settings = new Settings()
+                    {
+                        FinishingSoundFilePath = selectedFilePath
+                    };
+                    _settingViewModel.FinishingSoundFilePath = selectedFilePath;
+                }
+                else
+                {
+                    MessageBox.Show("The selected file is not a valid WAV file. Please make sure to select an authentic WAV file and avoid renaming or changing the extension of an MP3 file to WAV. You can use MP3 to WAV Converter online.", "Invalid WAV file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool IsValidWavFile(string filePath)
+        {
+            try
+            {
+                using(FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using(BinaryReader reader = new BinaryReader(fs))
+                    {
+                        // Check the RIFF header
+                        string riff = new string(reader.ReadChars(4));
+                        if(riff != "RIFF")
+                            return false;
+
+                        reader.ReadInt32(); // Skip 4 bytes (file size)
+
+                        // Check the WAVE header
+                        string wave = new string(reader.ReadChars(4));
+                        if(wave != "WAVE")
+                            return false;
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
